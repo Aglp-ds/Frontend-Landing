695cd728c9dbad8842752e2c0d0f33e6cf9dc62e
Aglp-ds <larezangelp@gmail.com>
2026-05-14 23:54:45 -0600

    first commit

---
+++ app/hola-mundo/page.tsx
@@ -0,0 +1,9 @@
+export default function holaMundo(){
+    return (
+        <main>
+            <div>
+                <h1>HOLA MUNDO</h1>
+            </div>
+        </main>
+    );
+}
---
+++ app/juan/page.tsx
@@ -0,0 +1,13 @@
+export default function juan (){
+    return(
+
+        <main>
+            <div><h1>Genes mi amor, tengamos 1000 hijos, att ANGEL JOSUE LAREZ PALACIOS</h1></div> 
+
+        </main>
+       
+
+
+    );
+
+}
